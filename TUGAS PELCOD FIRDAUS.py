bb8cb5972356505ab62c5354b3b23154fad8be23
FirdausNurH <144222509+FirdausNurH@users.noreply.github.com>
2023-09-07 11:01:33 +0700

    Add files via upload

---
+++ TUGAS PELCOD FIRDAUS.py
@@ -0,0 +1,13 @@
+#celcius
+
+print("mencari hasil celcius")
+c = int(input("angka : "))
+
+c = 9/5 * c + 32
+print("hasil : ",c,"c")
+
+print("mencari hasil fahrenheit")
+f = int(input("angka : "))
+
+f = 5/9 * f -32
+print("hasil : ",f,"f")
